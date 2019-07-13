363ab3264c932638f09bc41f8fcb7c1a9682d135
surelinks <fumsamuel@gmail.com>
2019-07-13 22:53:51 +0100

    chane sign in

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://git.heroku.com/shrouded-peak-26275.git/signin', {
+        fetch('https://shrouded-peak-26275.herokuapp.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
